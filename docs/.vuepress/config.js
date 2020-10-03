927524e862fd8111aa2ac430fbe3f60744b76224
Alexius Academia <alexius.academia@gmail.com>
2020-10-04 05:58:09 +0800

    Basic config file with title and nav

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    title: 'PSMS Documentation',
+    description: 'Detailed guides and documentation for PSMS.',
+    themeConfig: {
+        nav: [
+            {
+                text: 'Home',
+                link: '/'
+            },
+            {
+                text: 'Data Input',
+                link: '/input/'
+            },
+            {
+                text: 'Reporting',
+                link: '/reporting/'
+            },
+            {
+                text: 'PSMS',
+                link: 'https://psms.ph'
+            }
+        ]
+    }
+}
