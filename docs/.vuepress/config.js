979b48c20f56aa332897fdd739cf01d04ae43dd5
Alexius Academia <alexius.academia@gmail.com>
2020-10-04 16:20:12 +0800

    Applying sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,24 +1,32 @@
 module.exports = {
-    title: 'PSMS Documentation',
-    description: 'Detailed guides and documentation for PSMS.',
-    themeConfig: {
-        nav: [
-            {
-                text: 'Home',
-                link: '/'
-            },
-            {
-                text: 'Data Input',
-                link: '/input/'
-            },
-            {
-                text: 'Reporting',
-                link: '/reporting/'
-            },
-            {
-                text: 'PSMS',
-                link: 'https://psms.ph'
-            }
+  title: "PSMS Documentation",
+  description: "Detailed guides and documentation for PSMS.",
+  themeConfig: {
+    nav: [
+      {
+        text: "Home",
+        link: "/",
+      },
+      {
+        text: "Data Input",
+        link: "/input/",
+      },
+      {
+        text: "Reporting",
+        link: "/reporting/",
+      },
+      {
+        text: "PSMS",
+        link: "https://psms.ph",
+      },
+    ],
+
+    sidebar: {
+        '/input/': [
+            '',
+            'project',
+            'contract'
         ]
     }
-}
+  },
+};
---
+++ docs/input/README.md
@@ -0,0 +1,3 @@
+# Data Input
+
+This section will discuss about the guide on the different user inputs for the PSMS. For reporting, see the [Reporting](/reporting) tab.
